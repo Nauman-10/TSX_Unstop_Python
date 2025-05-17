dbdca680b5e599fbdbb014d45630b197d0e4e4f9
Nauman-10 <naumans0007@gmail.com>
2025-05-18 00:18:09 +0530

    Added comments.py

---
+++ comments.py
@@ -0,0 +1,3 @@
+# This script prints a greeting message.
+print("Hello, my name is Md Nauman Ansari.") # This line prints the name.
+print("Welcome to the world of Python programming.") # This line prints the welcome message.
