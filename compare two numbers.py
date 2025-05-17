4df1cb4f4eb4c1f8de5feef9654a25664fafd2f8
Nauman-10 <naumans0007@gmail.com>
2025-05-18 00:16:52 +0530

    Added compare two numbers.py

---
+++ compare two numbers.py
@@ -0,0 +1,10 @@
+# This script takes two numbers as input and prints whether the first number is greater, less than, or equal to the second number.
+a = 485
+b = 389
+
+if a > b:
+    print("First number is greater.")
+elif a < b:
+    print("Second number is greater.")
+else:
+    print("Both numbers are equal.")
