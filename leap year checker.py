ac5572d6defe9518dcacd7500d5ab7d4be319d79
Nauman-10 <naumans0007@gmail.com>
2025-05-18 00:19:48 +0530

    Added leap year checker.py

---
+++ leap year checker.py
@@ -0,0 +1,7 @@
+# This script checks if a given year is a leap year or not
+year = 2025
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
