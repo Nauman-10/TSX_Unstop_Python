f6ad6db778234098d9b8138ba11851d4ba638c8b
Nauman-10 <naumans0007@gmail.com>
2025-05-18 00:25:52 +0530

    Added string concatenation.py

---
+++ string concatenation.py
@@ -0,0 +1,6 @@
+# This script concatenates two strings to form a full name.
+first_name = "Md Nauman"
+last_name = "Ansari"
+
+full_name = first_name + " " + last_name
+print("Full Name:", full_name)
