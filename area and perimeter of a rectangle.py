c627b5fc0b74cbdc0ed72d9184287aa93cb4becf
Nauman-10 <naumans0007@gmail.com>
2025-05-18 00:23:35 +0530

    Added area and perimeter of a rectangle.py

---
+++ area and perimeter of a rectangle.py
@@ -0,0 +1,7 @@
+# This script calculates the area and perimeter of a rectangle
+length = 8
+breadth = 5
+area = length * breadth
+perimeter = 2 * (length + breadth)
+print("Area of the rectangle is:", area)
+print("Perimeter of the rectangle:", perimeter)
